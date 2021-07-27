cf2fdd631acc0cdb276b233eeb4661e05df65e74
kebufu <mcku_hei@qq.com>
2021-07-27 17:22:53 +0800

    update.

--- src/exploresurvival/game/ExploreSurvival.java
+++ src/exploresurvival/game/ExploreSurvival.java
@@ -3,6 +3,8 @@ package exploresurvival.game;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JFrame;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -16,6 +18,7 @@ import exploresurvival.game.gui.GuiMainMenu;
 import exploresurvival.game.render.FontRenderer;
 import exploresurvival.game.render.RenderEngine;
 import exploresurvival.game.util.GameSettings;
+import exploresurvival.game.util.PanelCrashReport;
 import exploresurvival.game.util.ScaledResolution;
 
 public class ExploreSurvival extends Thread {
@@ -48,8 +51,10 @@ public class ExploreSurvival extends Thread {
 			currentScreen.onClose();
 		}
 		currentScreen=screen;
-		ScaledResolution sr=new ScaledResolution();
-		screen.onOpen(sr.getScaledWidth(), sr.getScaledHeight());
+		if(screen!=null) {
+			ScaledResolution sr=new ScaledResolution();
+			screen.onOpen(sr.getScaledWidth(), sr.getScaledHeight());
+		}
 	}
 	private void init() throws LWJGLException {
 		fullscreen=false;
@@ -104,75 +109,81 @@ public class ExploreSurvival extends Thread {
 		int frames = 0;
 		try {
 			init();
-		} catch (LWJGLException e) {
+		} catch (Throwable e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.exit(-1);
+			new PanelCrashReport(e);
+			running=false;
 		}
-		while(running) {
-			start=System.currentTimeMillis();
-			GL11.glClear(256);
-			if(width!=Display.getWidth()||height!=Display.getHeight()) {
-				width=Display.getWidth();
-				height=Display.getHeight();
-				ScaledResolution sr=new ScaledResolution();
-				GL11.glViewport(0, 0, width, height);
+		try {
+			while(running) {
+				start=System.currentTimeMillis();
 				GL11.glClear(256);
-	            GL11.glMatrixMode(5889);
-	            GL11.glLoadIdentity();
-	            GL11.glOrtho(0.0D, sr.field_25121_a, sr.field_25120_b, 0.0D, 100.0D, 300.0D);
-	            GL11.glMatrixMode(5888);
-	            GL11.glLoadIdentity();
-	            GL11.glTranslatef(0.0F, 0.0F, -200.0F);
-	            if(currentScreen!=null) {
-	            	currentScreen.controls.clear();
-	            	currentScreen.onOpen(sr.getScaledWidth(), sr.getScaledHeight());
-	            }
-			}
-			GL11.glPushMatrix();
-			GL11.glEnable(GL11.GL_TEXTURE_2D);
-			GL11.glEnable(GL11.GL_ALPHA_TEST);
-			GL11.glEnable(GL11.GL_BLEND);
-            if(currentScreen!=null) {
-            	ScaledResolution sr=new ScaledResolution();
-                int screenWidth = sr.getScaledWidth();
-                int screenHeight = sr.getScaledHeight();
-                int xMouse = Mouse.getX() * screenWidth / this.width;
-                int yMouse = screenHeight - Mouse.getY() * screenHeight / this.height - 1;
-                GL11.glColor4f(1F, 1F, 1F, 1F);
-            	currentScreen.render(xMouse, yMouse);
-            	currentScreen.tick();
-            } else {
-            	while(Mouse.next()) {
-    				
-    			}
-    			while(Keyboard.next()) {
-    				char c=Keyboard.getEventCharacter();
-    				int key=Keyboard.getEventKey();
-    				if(key==Keyboard.KEY_F11) {
-    					toggleFullscreen();
-    				}
-    			}
-            }
-			fontrenderer.render("ExploreSurvival ("+this.frames+" frames)", 2, 2, 0xFFFFFF);
-			GL11.glPopMatrix();
-			checkGlError("render 2d");
-			if(gamesettings.limitFrames) {
-				try {
-					sleep((long) Math.max((1F/maxframes*1000F)-(System.currentTimeMillis()-start), 0));
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				if(width!=Display.getWidth()||height!=Display.getHeight()) {
+					width=Display.getWidth();
+					height=Display.getHeight();
+					ScaledResolution sr=new ScaledResolution();
+					GL11.glViewport(0, 0, width, height);
+					GL11.glClear(256);
+		            GL11.glMatrixMode(5889);
+		            GL11.glLoadIdentity();
+		            GL11.glOrtho(0.0D, sr.field_25121_a, sr.field_25120_b, 0.0D, 100.0D, 300.0D);
+		            GL11.glMatrixMode(5888);
+		            GL11.glLoadIdentity();
+		            GL11.glTranslatef(0.0F, 0.0F, -200.0F);
+		            if(currentScreen!=null) {
+		            	currentScreen.controls.clear();
+		            	currentScreen.onOpen(sr.getScaledWidth(), sr.getScaledHeight());
+		            }
 				}
+				GL11.glPushMatrix();
+				GL11.glEnable(GL11.GL_TEXTURE_2D);
+				GL11.glEnable(GL11.GL_ALPHA_TEST);
+				GL11.glEnable(GL11.GL_BLEND);
+	            if(currentScreen!=null) {
+	            	ScaledResolution sr=new ScaledResolution();
+	                int screenWidth = sr.getScaledWidth();
+	                int screenHeight = sr.getScaledHeight();
+	                int xMouse = Mouse.getX() * screenWidth / this.width;
+	                int yMouse = screenHeight - Mouse.getY() * screenHeight / this.height - 1;
+	                GL11.glColor4f(1F, 1F, 1F, 1F);
+	            	currentScreen.render(xMouse, yMouse);
+	            	currentScreen.tick();
+	            } else {
+	            	while(Mouse.next()) {
+	    				
+	    			}
+	    			while(Keyboard.next()) {
+	    				char c=Keyboard.getEventCharacter();
+	    				int key=Keyboard.getEventKey();
+	    				if(key==Keyboard.KEY_F11) {
+	    					toggleFullscreen();
+	    				}
+	    			}
+	            }
+				fontrenderer.render("ExploreSurvival ("+this.frames+" frames)", 2, 2, 0xFFFFFF);
+				GL11.glPopMatrix();
+				checkGlError("render 2d");
+				if(gamesettings.limitFrames) {
+					try {
+						sleep((long) Math.max((1F/maxframes*1000F)-(System.currentTimeMillis()-start), 0));
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				frames++;
+				if(System.currentTimeMillis()-l>=1000L) {
+					this.frames=frames;
+					frames=0;
+					l=System.currentTimeMillis();
+				}
+				running=running&&!Display.isCloseRequested();
+				Display.update();
 			}
-			frames++;
-			if(System.currentTimeMillis()-l>=1000L) {
-				this.frames=frames;
-				frames=0;
-				l=System.currentTimeMillis();
-			}
-			running=running&&!Display.isCloseRequested();
-			Display.update();
+		} catch(Exception e) {
+			e.printStackTrace();
+			new PanelCrashReport(e);
 		}
 	}
     public void toggleFullscreen() {
