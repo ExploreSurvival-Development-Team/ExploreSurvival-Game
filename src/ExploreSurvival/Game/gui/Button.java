fd4a72db600fa70959ab4d5139f0fa2a83fa5966
kebufu <mcku_hei@qq.com>
2021-07-27 11:54:09 +0800

    update.

---
+++ src/ExploreSurvival/Game/gui/Button.java
@@ -0,0 +1,71 @@
+package exploresurvival.game.gui;
+
+import org.lwjgl.opengl.GL11;
+
+import exploresurvival.game.ExploreSurvival;
+import exploresurvival.game.render.FontRenderer;
+import exploresurvival.game.render.RenderEngine;
+
+public class Button extends Component {
+	// w: 按钮宽度 h:按钮高度
+	int x,y,w,h;
+	public boolean enable,visible;
+	String text;
+	public Button(ExploreSurvival game, int x, int y, String text) {
+		this(game,x,y,200,20, text);
+	}
+
+	public Button(ExploreSurvival game, int x2, int y2, int i, int j, String text) {
+		super(game);
+		this.x=x2;
+		this.y=y2;
+		this.w=i;
+		this.h=j;
+		enable=true;
+		visible=true;
+		this.text=text;
+	}
+
+	@Override
+	public void render(int mouseX, int mouseY) {
+		if(visible) {
+			FontRenderer font=game.fontrenderer;
+			RenderEngine engine=game.renderengine;
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, engine.getTexture("/gui.png"));
+			if(!enable) {
+				engine.drawImage(x, y, 0, 46, w / 2, h);
+				engine.drawImage(x+w / 2, y, 200 - this.w / 2, 46, w / 2, h);
+				RenderEngine.drawCenteredString(font, text, x+w/2, y+(h/2-4), 0xa0a0a0);
+			}
+			boolean flag = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.w && mouseY < this.y + this.h;
+			engine.drawImage(x, y, 0, flag? 86 : 66, w / 2, h);
+			engine.drawImage(x+w / 2, y, 200 - this.w / 2, flag? 86 : 66, w / 2, h);
+			RenderEngine.drawCenteredString(font, text, x+w/2, y+(h/2-4), flag ? 0xffffa0 : 0xe0e0e0);
+		}
+	}
+
+	@Override
+	public void onClose() {
+		
+	}
+
+	@Override
+	public void onMouseClick(int mouseX, int mouseY, int eventButton, GuiScreen screen) {
+		if(eventButton==0) {
+			boolean flag = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.w && mouseY < this.y + this.h;
+			if(flag) screen.componentEvent(this);
+		}
+	}
+
+	@Override
+	public void onKeyPress(char eventCharacter, int eventKey, GuiScreen screen) {
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
