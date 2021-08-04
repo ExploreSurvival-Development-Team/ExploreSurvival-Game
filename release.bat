8139719cef9df164b1d06852826d48a13c37c8ec
zhou2008 <493505110@qq.com>
2021-08-04 11:08:05 +0800

    null

--- release.bat
+++ release.bat
@@ -8,6 +8,13 @@ del /s /q bin\*.java
 del sources.txt
 cd bin
 jar -cvfm game.jar META-INF\MANIFEST.MF *
-move game.jar ..
 cd ..
+
+rd /s /q out
+md out
+md out\lib
+md out\natives
+xcopy lib\* out\lib
+xcopy natives\* out\natives
+move bin\game.jar out\
 pause
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -cp bin;lib\*;. -Djava.library.path=natives exploresurvival.game.ExploreSurvival
-pause
